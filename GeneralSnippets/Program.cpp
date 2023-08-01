ee221d0ef01ad55958ceb373af5e250289ed27b3
Peter Loos <peter.loos@gmx.de>
2023-08-01 17:00:37 +0200

    Stand Dienstag Abend.

--- GeneralSnippets/Program.cpp
+++ GeneralSnippets/Program.cpp
@@ -41,7 +41,7 @@ int main()
         //main_decltype();
         //main_default_initialization();
         //main_erase_remove_idiom();
-        //main_exercises();
+        main_exercises();
         //main_expression_templates();
         //main_generate();
         //main_generic_lambdas();
@@ -95,7 +95,7 @@ int main()
         //main_variadic_templates_working_on_every_argument();
         //main_variadic_templates_sum_of_sums();
         //main_variadic_templates_mixins();
-        main_variant();
+        //main_variant();
         //main_virtual_base_class_destructor();
         //main_virtual_override_final();
         //main_weak_pointer();
